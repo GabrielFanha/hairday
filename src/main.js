d25ed73509400189d6d85e5fdf6d2de5a576acf5
Gabriel Fanha <gabriel.lfanha@outlook.com>
2025-02-16 11:24:10 -0300

    set data form

--- src/main.js
+++ src/main.js
@@ -10,6 +10,9 @@ import "./styles/form.css"
 import "./styles/index.css"
 import "./styles/schedule.css"
 
+//JS
+import "./modules/form/submit.js"
+
 import dayjs from "dayjs"
 
 console.log(dayjs().format("HH:mm"))
---
+++ src/modules/form/submit.js
@@ -0,0 +1,19 @@
+import dayjs from "dayjs"
+
+const form = document.querySelector("form")
+const selectedDate = document.getElementById("date")
+
+const today = dayjs(new Date()).format("YYYY-MM-DD")
+
+//Carrega data atual
+selectedDate.value = today
+
+//Define a data minima para agendar
+selectedDate.min = today
+
+form.onsubmit = (event) => {
+  // Prevent reload form
+  event.preventDefault()
+
+
+}
