8fc6a2f04de9446b0ddbd27929d3e478d7a19d71
Gabriel Fanha <gabriel.lfanha@outlook.com>
2025-02-16 14:28:14 -0300

    divid hours to period

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -8,7 +8,10 @@ export function hoursLoad({ date }) {
     const [scheduleHour, ] = hour.split(":")
 
     const isHourPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs())
-
+    console.log({
+      hour,
+      available: isHourPast
+    })
     return {
       hour,
       available: isHourPast
@@ -18,8 +21,23 @@ export function hoursLoad({ date }) {
   opening.forEach(({ hour, available }) => {
     const li = document.createElement("li")
     li.classList.add("hour")
-    li.classList.add(available ? "hour-avaliable" : "hour-unavaliable")
+    li.classList.add(available ? "hour-avaliable" : "hour-unavailable")
     li.textContent = hour
+
+    if(hour === "9:00"){
+      hourHeaderAdd("Manhã")
+    }else if (hour === "13:00") {
+      hourHeaderAdd("Tarde")
+    } else if (hour === "18:00") {
+      hourHeaderAdd("Noite")
+    }
     hours.append(li)
   })
+}
+
+function hourHeaderAdd(title) {
+  const header = document.createElement("li")
+  header.classList.add("hour-period")
+  header.textContent = title
+  hours.append(header)
 }
