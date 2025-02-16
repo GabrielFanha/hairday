b0596ceca220f58dd3487c916ebf6cac5cc6dc1e
Gabriel Fanha <gabriel.lfanha@outlook.com>
2025-02-16 14:20:06 -0300

    function create item hour

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,6 +1,8 @@
 import dayjs from "dayjs"
 import { openingHours } from "../../utils/opening-hours.js"
 
+const hours = document.getElementById("hours")
+
 export function hoursLoad({ date }) {
   const opening = openingHours.map((hour) => {
     const [scheduleHour, ] = hour.split(":")
@@ -11,5 +13,13 @@ export function hoursLoad({ date }) {
       hour,
       available: isHourPast
     }
+  })
+
+  opening.forEach(({ hour, available }) => {
+    const li = document.createElement("li")
+    li.classList.add("hour")
+    li.classList.add(available ? "hour-avaliable" : "hour-unavaliable")
+    li.textContent = hour
+    hours.append(li)
   })
 }
