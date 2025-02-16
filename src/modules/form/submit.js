335c22480152d90fb09629fe8d5b263d3c815ef7
Gabriel Fanha <gabriel.lfanha@outlook.com>
2025-02-16 14:57:10 -0300

    update function submit

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -2,6 +2,8 @@ import dayjs from "dayjs"
 
 const form = document.querySelector("form")
 const selectedDate = document.getElementById("date")
+const clientName = document.getElementById("client")
+
 
 const today = dayjs(new Date()).format("YYYY-MM-DD")
 
@@ -15,5 +17,25 @@ form.onsubmit = (event) => {
   // Prevent reload form
   event.preventDefault()
 
-  
+  try {
+    const name = clientName.value.trim()
+    if (!name) {
+      return alert(`Informe o nome do cliente!`)
+    }
+
+    const hourSelected = document.querySelector(".hour-selected")
+    console.log(hourSelected)
+    if(!hourSelected) {
+      return alert(`Selecione a hora!`)
+    }
+    const [hour,] = hourSelected.innerText.split(":")
+
+    const when = dayjs(selectedDate.value).add(hour, "hour")
+    const id = new Date().getTime()
+
+    console.log({ id, name, when })
+  } catch (error) {
+    alert(`Não foi possível realizar o agendamento.`)
+    console.log(error)
+  }
 }
