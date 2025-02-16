cfd2303ac1d8463b91039d1d03b39e04dffd86f0
Gabriel Fanha <gabriel.lfanha@outlook.com>
2025-02-16 11:07:10 -0300

    add dayjs

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 "use strict"
 
+// Config DayJS
+
+import "./libs/dayjs.js"
+
+// CSS
 import "./styles/global.css"
 import "./styles/form.css"
 import "./styles/index.css"
 import "./styles/schedule.css"
+
+import dayjs from "dayjs"
+
+console.log(dayjs().format("HH:mm"))
