606aa4dce1401046acc1c9fe80311e0bc2a74732
Gabriel Fanha <gabriel.lfanha@outlook.com>
2025-02-17 18:21:29 -0300

    add function scheduleByDay

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,11 +1,12 @@
 import { hoursLoad } from "../form/hours-load.js";
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js";
 
 const selectedDate = document.getElementById("date")
 
-export function schedulesDay() {
+export async function schedulesDay() {
   const date = selectedDate.value
 
-  
+  const dailySchedules = await scheduleFetchByDay({ date })
 
   hoursLoad({date})
 }
--- src/services/schedule-fetch-by-day.js
+++ src/services/schedule-fetch-by-day.js
@@ -1,12 +1,12 @@
 import dayjs from "dayjs"
-import { apiConfig } from "./api.config"
+import { apiConfig } from "./api.config.js"
 
 
 export async function scheduleFetchByDay({ date }) {
   try {
     const response = await fetch(`${apiConfig.baseURL}/schedules`)
     const data = await response.json()
-    const dailySchedules = data.filter((schedule) => dayjs(date).isSame(schedule.when, "day"))
+    const dailySchedules = await data.filter((schedule) => dayjs(date).isSame(schedule.when, "day"))
     return dailySchedules
   } catch (error) {
     console.log(error)
